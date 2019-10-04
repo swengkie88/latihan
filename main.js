33a6c52b14b698077066d7d91b1d1685586d035c
swengkie88 <angkiprana1987@gmail.com>
2019-10-04 17:22:52 +0700

    Bikin kalkulator sederhana

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $('#banner').text("Kulo Nuwun Donya!");
-function sapadia(){
-	var nama = $('#nama').val();
-	$('#banner').text(`Hello, ${nama}!`);
+function tambahkan(){
+	var angka1 = parseInt($('#angka1').val());
+	var angka2 = parseInt($('#angka2').val());
+	var hasil = angka1 + angka2;
+	$('#hasil').text(`Hasil dari ${angka1} + ${angka2} = ${hasil}`);
 }
